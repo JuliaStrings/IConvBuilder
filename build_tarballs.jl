ee99ea93926da65a7cc0c3837f57ccf74fe617b3
David Anthoff <anthoff@berkeley.edu>
2018-05-18 20:46:16 -0700

    Update to latest BinaryBuilder.jl syntax

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,25 +1,5 @@
 using BinaryBuilder
 
-# These are the platforms built inside the wizard
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-  BinaryProvider.Linux(:x86_64, :glibc),
-  BinaryProvider.Linux(:aarch64, :glibc),
-  BinaryProvider.Linux(:armv7l, :glibc),
-  BinaryProvider.Linux(:powerpc64le, :glibc),
-  BinaryProvider.MacOS(),
-  BinaryProvider.Windows(:i686),
-  BinaryProvider.Windows(:x86_64)
-]
-
-
-# If the user passed in a platform (or a few, comma-separated) on the
-# command-line, use that instead of our default platforms
-if length(ARGS) > 0
-    platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(platforms), ", "))")
-
 # Collection of sources required to build libiconv
 sources = [
     "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz" =>
@@ -29,24 +9,24 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd libiconv-1.15/
-./configure --prefix=/ --host=$target
-make
+./configure --prefix=$prefix --host=${target}
+make -j${nproc}
 make install
-
 """
 
+# These are the platforms we will build for by default, unless
+# further platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
 products = prefix -> [
-    LibraryProduct(prefix,"libcharset"),
-    LibraryProduct(prefix,"libiconv"),
-    ExecutableProduct(prefix,"iconv")
+    LibraryProduct(prefix,"libcharset",:libcharset),
+    LibraryProduct(prefix,"libiconv",:libiconv),
+    ExecutableProduct(prefix,"iconv",:iconv)
 ]
 
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
 
-# Build the given platforms using the given sources
-hashes = autobuild(pwd(), "libiconv", platforms, sources, script, products)
-
-if !isempty(get(ENV,"TRAVIS_TAG",""))
-    print_buildjl(pwd(), products, hashes,
-        "https://github.com/davidanthoff/IConvBuilder/releases/download/$(ENV["TRAVIS_TAG"])")
-end
-
+build_tarballs(ARGS, "Snappy", sources, script, platforms, products, dependencies)
